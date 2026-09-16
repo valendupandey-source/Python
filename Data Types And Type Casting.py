26466c5fe030ee6afd20c8053b7f7290b9fdb84d
valendupandey-source <valendupandey@gmail.com>
2026-09-16 18:49:08 +0530

    Add files via upload

---
+++ Data Types And Type Casting.py
@@ -0,0 +1,19 @@
+name = "Valendu"
+age = 11
+is_student = True
+weight = 33
+print ("Name : ", name)
+print ("Data type of name is : ", type(name))
+print ("Age : ", age)
+print ("Data type of Age is : ", type(age))
+print ("is_student : ", is_student)
+print ("Data type of is_student is : ", type(is_student))
+print ("Weight : ", weight)
+print ("Data type of Weight is : ", type(weight))
+print ("\ n after type casting.....")
+age = str(age)
+print (age)
+print ("Data type of Age is : ", type(age))
+weight = int(weight)
+print (weight)
+print ("Data type of Weight is : ", type(weight))
