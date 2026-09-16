065c3f1c799624b2c9e7641f9244ec4472548dc5
valendupandey-source <valendupandey@gmail.com>
2026-09-16 19:01:46 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,11 @@
+num1 = 100
+num2 = 67
+print ("Number1", num1)
+print ("Number2", num2)
+print ("Addition :", num1 + num2)
+print ("Subtraction :", num1 - num2)
+print ("Product :", num1 * num2)
+print ("Division :", num1 / num2)
+print ("Floor Division :", num1 // num2)
+print ("Modulus Operation :", num1 % num2)
+print ("Square :", num1 ** num2)
