8a231ef28c56b62fa2fd8632951d37ca92e31e2d
valendupandey-source <valendupandey@gmail.com>
2026-09-17 17:27:50 +0530

    Add files via upload

---
+++ Conversation With Strings.py
@@ -0,0 +1,16 @@
+st1 = " My Name Is Valendu Pandey "
+st2 = " My Age Is 11 "
+st3 = " I Study In Class 6 "
+join_st = st1 + st2 + st3
+print(join_st)
+print ( " The length of all 3 sentences is",len(st1) + len(st2) + len(st3) )
+
+num1 = 456
+num2 = 573
+sum = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+div = num1 / num2
+floor_div = num1 // num2
+expon = num1 ** num2
+print (sum,diff,prod, div, floor_div," Exponent => ",expon," <= Exponent ")
