137493bea0107adbfb346ae69652b7971f17a7e8
valendupandey-source <valendupandey@gmail.com>
2026-09-17 17:03:16 +0530

    Add files via upload

---
+++ Play With Strings.py
@@ -0,0 +1,15 @@
+first_name = "Valendu"
+last_name = "Pandey"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = 'Kings'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[3])
+print("String Sliced :", word[0:3])
