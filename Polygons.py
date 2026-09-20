5b1d35a977a8a126f9aaa87c36092c89c172870f
valendupandey-source <valendupandey@gmail.com>
2026-09-20 18:55:51 +0530

    Add files via upload

---
+++ Polygons.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("Maroon")
+
+board = turtle.Turtle()
+ 
+# Triangle
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+
+
+# Diamond
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
