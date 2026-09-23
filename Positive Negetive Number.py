25ab16b0474fcdbf0cca0273cb9cf12cdb1881df
valendupandey-source <valendupandey@gmail.com>
2026-09-23 18:41:52 +0530

    Add files via upload

---
+++ Positive Negetive Number.py
@@ -0,0 +1,9 @@
+number = int(input("Enter No. To Check"))
+print ("Number to be checked : ", number)
+if number > 0:
+    print("This is a positive number")
+elif number < 0:
+    print("This is a negative integer")    
+else:
+    print ("This is neutral")    
+    
