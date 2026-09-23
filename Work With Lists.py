4cefcd8989e4040843ad585bd7ce5d477481b3dc
valendupandey-source <valendupandey@gmail.com>
2026-09-23 18:59:46 +0530

    Add files via upload

---
+++ Work With Lists.py
@@ -0,0 +1,9 @@
+lst = ['Apple','Guava','Mango','Banana','Kiwi']
+print ("Length of list",len(lst))
+print ("First Element",lst[0])
+print ("Last Element",lst[-1])
+lst.append('Papaya')
+print ("Updated List : ", lst )
+lst.remove('Banana')
+print ("Sorted List : ", lst)
+
