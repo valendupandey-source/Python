679d3f3918e8f3159770e0ae35bae06c7c7d8d20
valendupandey-source <valendupandey@gmail.com>
2026-09-24 17:07:05 +0530

    Add files via upload

---
+++ Natural Numbers.py
@@ -0,0 +1,8 @@
+number = int(input(" Check A Natural Number Or Not "))
+print ("Number to be checked", number)
+if number  > 0:
+    print (" This is a Natural Number ")
+elif number < 0:
+    print (" This is not a Natural Number ")    
+else:
+    print (" This Number Is 0 Itself ")        
